4e3976e51fdf2a0c3507ce83e1c0ed4493dc7662
bun919tw <bun919tw@gmail.com>
2016-07-21 22:03:25 +0800

    Get color transactions API
    * Refactor the test file of explorer module.

--- oss_server/explorer/v1/views.py
+++ oss_server/explorer/v1/views.py
@@ -1,5 +1,6 @@
 import httplib
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import JsonResponse
 from django.views.generic import View
 
@@ -41,3 +42,31 @@ class GetTxByHashView(View):
         except Tx.DoesNotExist:
             response = {'error': 'tx not exist'}
             return JsonResponse(response, status=httplib.NOT_FOUND)
+
+
+class GetColorTxsView(View):
+    def get(self, request, color_id):
+        tx_list = Tx.objects.filter(tx_out__color=color_id, type__lte=1).distinct()
+        tx_list = [tx.as_dict() for tx in tx_list]
+        paginator = Paginator(tx_list, 50)
+
+        # deliver first page if page is not given
+        if 'page' in request.GET:
+            page = request.GET['page']
+        else:
+            page = 1
+
+        try:
+            txs = paginator.page(page)
+        except PageNotAnInteger:
+            # deliver first page if page is not an integer
+            txs = paginator.page(1)
+            page = 1
+        except EmptyPage:
+            # deliver last page if page is out of range
+            txs = paginator.page(paginator.num_pages)
+            page = paginator.num_pages
+
+        response = {'page': page, 'txs': txs.object_list}
+        return JsonResponse(response)
+
