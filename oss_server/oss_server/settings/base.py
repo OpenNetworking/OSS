ee986127b2c53690f2c735b1153a9a1685a4795e
Jsying <jsying13@gmail.com>
2016-08-25 05:49:59 +0000

    Adjust indentation

--- oss_server/oss_server/settings/base.py
+++ oss_server/oss_server/settings/base.py
@@ -138,12 +138,12 @@ STATIC_URL = '/static/'
 LOG_DIR =  os.path.dirname(BASE_DIR) + '/log/'
 LOGGING = {
     'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'verbose': {
-                'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-                'datefmt' : "%d/%b/%Y %H:%M:%S"
-            },
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
     },
     'handlers': {
         'file': {
